463a96a3499a25e2066cbb180dfbe81c82adc304
esteban1318 <poveda1318@gmail.com>
2025-06-13 16:59:39 -0500

    correciones en apiApplication

--- src/main/java/com/miempresa/api/ApiApplication.java
+++ src/main/java/com/miempresa/api/ApiApplication.java
@@ -40,18 +40,5 @@ public class ApiApplication implements CommandLineRunner {
 			logger.error("❌ Error al conectar con la base de datos: {}", e.getMessage());
 		}
 	}
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry){
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:3000")
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-						.allowedHeaders("*")
-						.allowCredentials(true);
 
-			}
-		};
-	}
 }
